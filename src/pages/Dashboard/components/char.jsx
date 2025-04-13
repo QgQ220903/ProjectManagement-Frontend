e9bf91aff58767640c7b4ea4b97a1b35e9dae082
Tran Truong <chantruong753@gmail.com>
2025-04-13 12:08:07 +0700

    code

--- src/pages/Dashboard/components/char.jsx
+++ src/pages/Dashboard/components/char.jsx
@@ -3,22 +3,7 @@ import { Table } from 'antd';
 import EmptyTemplate from "@/components/emptyTemplate/EmptyTemplate";
 
 export const LineChartDb = ({data}) => (
-  <LineChart width={700} height={400} data={[...data,
-    {
-    department_name: "phòng ban B",
-    done: "10",
-    delayed: "2",
-    total_tasks: "20",
-    in_process:"8",
-  },
-  {
-    department_name: "phòng ban C",
-    done: "4",
-    delayed: "2",
-    total_tasks: "15",
-    in_process:"9"
-  }
-  ]}>
+  <LineChart width={700} height={400} data={data}>
     <XAxis dataKey="department_name" />
     <YAxis />
     <CartesianGrid strokeDasharray="3 3" />
@@ -26,7 +11,7 @@ export const LineChartDb = ({data}) => (
     <Legend />
     <Line type="monotone" dataKey="done" stroke="#8884d8"  name="Công việc hoàn thành"/>
     <Line type="monotone" dataKey="total_tasks" stroke="green"  name="Tổng Công việc"/>
-    <Line type="monotone" dataKey="delayed" stroke="red"  name="Công việc chua hoàn thành"/>
+    <Line type="monotone" dataKey="delayed" stroke="red"  name="Công việc chưa hoàn thành"/>
     <Line type="monotone" dataKey="in_process" stroke="orange"  name="Công việc đang làm"/>
   </LineChart>
 );
